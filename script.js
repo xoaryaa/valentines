fff65bef3c641021ca12c024516c58cb44dec4c1
Arya Tayshete <avtayshete_b21@et.vjti.ac.in>
2024-02-17 01:05:41 +0530

    Add files via upload
    second commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('yesBtn')?.addEventListener('click', function () {
+    window.location.href = 'pg2.html'; 
+});
+document.getElementById('noBtn')?.addEventListener('click', function () {
+    window.location.href = 'pg3.html';
+});
+document.getElementById('meBtn')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'pg4.html';
+});
+document.getElementById('submitBtn')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'pg5.html';
+});
+document.getElementById('buttonjap')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'jap.html';
+});
+document.getElementById('buttonthai')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'thai.html';
+});
+document.getElementById('buttonind')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'ind.html';
+});
+document.getElementById('buttonitaly')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'italy.html';
+});
+document.getElementById('buttonmex')?.addEventListener('click', function () {
+    console.log("hey")
+    window.location.href = 'mex.html';
+});
